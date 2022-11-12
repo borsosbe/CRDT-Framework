09aa5aa36ed0bc8a6e376e5714141d243dab2285
borsosbe <borsosbe@gmail.com>
2022-11-12 10:59:42 +0100

    Initial Commit

---
+++ CRDT-Framework/CRDT_Framework.h
@@ -0,0 +1,18 @@
+//
+//  CRDT_Framework.h
+//  CRDT-Framework
+//
+//  Created by Bence Borsos on 2022. 11. 12..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRDT_Framework.
+FOUNDATION_EXPORT double CRDT_FrameworkVersionNumber;
+
+//! Project version string for CRDT_Framework.
+FOUNDATION_EXPORT const unsigned char CRDT_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDT_Framework/PublicHeader.h>
+
+
